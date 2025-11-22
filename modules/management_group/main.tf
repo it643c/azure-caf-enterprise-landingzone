c5278924f583e95d9705e86ffa547f2e2b9f852d
Robert Blake <it643c@gmail.com>
2025-11-21 18:08:47 -0700

    feat: initial enterprise landing zone skeleton with CAF management group hierarchy, pinned providers, and remote state backend

---
+++ modules/management_group/main.tf
@@ -0,0 +1,34 @@
+resource "azurerm_management_group" "root" {
+  display_name = "Root"
+  name         = "root"
+}
+
+resource "azurerm_management_group" "decommissioned" {
+  display_name               = "Decommissioned"
+  parent_management_group_id = azurerm_management_group.root.id
+}
+
+resource "azurerm_management_group" "sandboxes" {
+  display_name               = "Sandboxes"
+  parent_management_group_id = azurerm_management_group.root.id
+}
+
+resource "azurerm_management_group" "landing_zones" {
+  display_name               = "Landing Zones"
+  parent_management_group_id = azurerm_management_group.root.id
+}
+
+resource "azurerm_management_group" "platform" {
+  display_name               = "Platform"
+  parent_management_group_id = azurerm_management_group.root.id
+}
+
+resource "azurerm_management_group" "corp" {
+  display_name               = "Corp"
+  parent_management_group_id = azurerm_management_group.landing_zones.id
+}
+
+resource "azurerm_management_group" "online" {
+  display_name               = "Online"
+  parent_management_group_id = azurerm_management_group.landing_zones.id
+}
